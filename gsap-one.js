61c5ea16e771ab761bef8691afc2f1ad7a6f3d2c
Saiful Alam Fahim <fahim12345saiful@gmail.com>
2024-01-09 20:36:36 +0600

    timeline

--- gsap-one.js
+++ gsap-one.js
@@ -7,23 +7,19 @@
 //     scale: 0.5,
 // });
 
-gsap.from("#box", {
+var tl = gsap.timeline();
+
+tl.to("h1", {
     x:700,
     duration: 1,
-    delay: 1,
-    stagger: 1,
-    repeat: -1,
-    yoyo: true,
 });
 
-// gsap.from("h2", {
-//     x:700,
-//     duration: 2,
-//     delay: 3,
-// });
+tl.to("h2", {
+    x:700,
+    duration: 2,
+});
 
-// gsap.from("h3", {
-//     x:700,
-//     duration: 2,
-//     delay: 5,
-// });
+tl.to("h3", {
+    x:700,
+    duration: 2,
+});
