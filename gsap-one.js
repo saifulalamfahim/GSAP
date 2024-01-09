a4eb31035e4866695ef5e567af6ad7713899b6ec
Saiful Alam Fahim <fahim12345saiful@gmail.com>
2024-01-09 20:07:58 +0600

    gsap from

--- gsap-one.js
+++ gsap-one.js
@@ -1,8 +1,26 @@
-gsap.to("#box", {
-    x: 900,
+// gsap.to("#box", {
+//     x: 900,
+//     duration: 2,
+//     delay: 1,
+//     backgroundColor: "blue",
+//     rotate: 360,
+//     scale: 0.5,
+// });
+
+gsap.from("h1", {
+    x:700,
     duration: 2,
     delay: 1,
-    backgroundColor: "blue",
-    rotate: 360,
-    scale: 0.5,
-})
+});
+
+gsap.from("h2", {
+    x:700,
+    duration: 2,
+    delay: 3,
+});
+
+gsap.from("h3", {
+    x:700,
+    duration: 2,
+    delay: 5,
+});
