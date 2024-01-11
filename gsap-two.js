7da385bdb8c9759561e1028e798c08902e5198e7
Saiful Alam Fahim <fahim12345saiful@gmail.com>
2024-01-11 19:37:35 +0600

    scroll trigger basic level complete

--- gsap-two.js
+++ gsap-two.js
@@ -6,11 +6,25 @@ gsap.from(".page-1 div", {
 gsap.from(".page-2 div", {
     rotate:360,
     duration: 2,
-    scrollTrigger: ".page-2 div"
+    scrollTrigger: {
+        trigger: ".page-2 div",
+        scroll:"body",
+        markers:true,
+        start: "top 80%",
+        end: "top 50%",
+        scrub: 3,
+    }
  })
 
  gsap.from(".page-3 div", {
     rotate:360,
     duration: 2,
-    scrollTrigger: ".page-3 div"
+    scrollTrigger: {
+        trigger: ".page-3 div",
+        scroll:"body",
+        markers:true,
+        start: "top 80%",
+        end: "top 50%",
+        scrub: 3,
+    }
  });
